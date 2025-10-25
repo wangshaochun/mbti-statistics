a2188b8b6ddee5507d2db1dd30bbde63b3192a47
wsc <wsc@qq.com>
2025-10-25 11:39:38 +0800

    remove image

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Calendar, Clock, User, Tag, ArrowLeft } from 'lucide-react';
 import { blogPosts, BlogPost } from '../../data/blogs';
 import Seo from '../../components/Seo';
