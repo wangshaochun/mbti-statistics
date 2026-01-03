b5eba3680a0e59cf626ca2091af0bae397ab17b5
wsc <wsc@qq.com>
2026-01-03 21:56:09 +0800

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import '../styles/globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -6,6 +7,19 @@ import Footer from '../components/Footer';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen bg-gray-50">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7V0CSS09LK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7V0CSS09LK');
+        `}
+      </Script>
       <Header />
       <main>
         <Component {...pageProps} />
